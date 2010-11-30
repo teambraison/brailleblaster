54bb9a02470b0cbc757b09dd7bada5d5b41cb76b
John Boyer <john.boyer@abilitiessoft.com>
2010-11-29 22:40:40 -0600

    working ant build file

--- src/main/org/brailleblaster/StartEnd.java
+++ src/main/org/brailleblaster/StartEnd.java
@@ -3,20 +3,20 @@ import org.brailleblaster.louisutdml.LouisFree;
 import org.brailleblaster.wordprocessor.WPManager;
 public class StartEnd
 {
-
 WPManager wpManager = new WPManager ();
 
 public static void main (String[] args)
 {
-if (args.length == 0)
-wpManager.normal ();
-else
-processArgs (args);
+StartEnd se = new StartEnd ();
+se.processArgs (args);
 LouisFree.louisFree ();
 }
 
 private void processArgs (String[] args)
 {
+if (args.length == 0)
+wpManager.normal ();
+else
 wpManager.special ();
 }
 
--- src/main/org/brailleblaster/louisutdml/CharToDots.java
+++ src/main/org/brailleblaster/louisutdml/CharToDots.java
@@ -2,12 +2,14 @@ package org.brailleblaster.louisutdml;
 import org.brailleblaster.jlouislibs.Jliblouisutdml;
 public class CharToDots
 {
-public String CharToDots (String inbuf, String config, int mode)
+public void charToDots (String trantab, String inbuf, String 
+outbuf, int mode)
 {
-byte [] inbufx;
-byte[] outbuf;
-String trantab;
+byte[] inbufx = null;
+byte[] outbufx = null;
+String logFile = null;
 Jliblouisutdml bindings = new Jliblouisutdml ();
-bindings.lbu_charToDots (trrantab, inbufx, outbuf, mode);
+bindings.lbu_charToDots (trantab, inbufx, outbufx, logFile, 
+mode);
 }
 }

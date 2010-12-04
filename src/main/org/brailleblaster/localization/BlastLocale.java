5a816c4ae0c5a5a5f7bd68cf959a37eb8b832e04
John Boyer <john.boyer@abilitiessoft.com>
2010-12-03 22:45:35 -0600

    just some more files

---
+++ src/main/org/brailleblaster/localization/BlastLocale.java
@@ -0,0 +1,14 @@
+package org.brailleblaster.localization;
+import java.util.*;
+
+public class BlastLocale extends ListResourceBundle
+{
+protected Object[][] getContent ()
+{
+return new Object[][] = 
+{
+{"file", "file"},
+{"edit", "edit"}
+};
+}
+}
--- src/main/org/brailleblaster/wordprocessor/TextEditor.java
+++ src/main/org/brailleblaster/wordprocessor/TextEditor.java
@@ -1,4 +1,8 @@
 package org.brailleblaster.wordprocessor;
+
+import org.eclipse.swt.*;
+import org.eclipse.swt.widgets.*;
+
 public class TextEditor
 {
 /** This is a bare-bones text editor for doing such things as creating 
@@ -6,6 +10,13 @@ and updating liblouis tables. It is included so as to take advantage of
 the cross-platform accessibility of BrailleBlaster. It can be invoked 
 only by a subcommand on the command line.
 */
+
+Display display = new Display();
+
+public boolean textEditor (String fileName)
+{
+return true;
+}
 
 }
 

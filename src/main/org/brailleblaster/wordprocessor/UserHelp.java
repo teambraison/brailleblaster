e985e6440354c28c0fb5193ed3bf65cf9dea56f8
John Boyer <john.boyer@abilitiessoft.com>
2011-09-28 10:05:12 -0500

    some work on menus

--- src/main/org/brailleblaster/wordprocessor/UserHelp.java
+++ src/main/org/brailleblaster/wordprocessor/UserHelp.java
@@ -1,8 +1,12 @@
 package org.brailleblaster.wordprocessor;
 
 import java.awt.Desktop;
+import org.brailleblaster.util.Notify;
 
 class UserHelp {
+UserHelp (String helpName) {
 Desktop desktop = Desktop.getDesktop();
+new Notify (helpName + " is being written.");
+}
 }
 

98612e7aed59bc04b33e82779407a12516956608
John Boyer <john.boyer@abilitiessoft.com>
2012-04-27 15:50:29 -0500

    added FileUtils class in  org.brailleblaster.utils

---
+++ src/main/org/brailleblaster/util/FileUtils.java
@@ -0,0 +1,82 @@
+/* BrailleBlaster Braille Transcription Application
+  *
+  * Copyright (C) 2010, 2012
+  * ViewPlus Technologies, Inc. www.viewplus.com
+  * and
+  * Abilitiessoft, Inc. www.abilitiessoft.com
+  * All rights reserved
+  *
+  * This file may contain code borrowed from files produced by various 
+  * Java development teams. These are gratefully acknoledged.
+  *
+  * This file is free software; you can redistribute it and/or modify it
+  * under the terms of the Apache 2.0 License, as given at
+  * http://www.apache.org/licenses/
+  *
+  * This file is distributed in the hope that it will be useful, but
+  * WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE
+  * See the Apache 2.0 License for more details.
+  *
+  * You should have received a copy of the Apache 2.0 License along with 
+  * this program; see the file LICENSE.
+  * If not, see
+  * http://www.apache.org/licenses/
+  *
+  * Maintained by John J. Boyer john.boyer@abilitiessoft.com
+*/
+package org.brailleblaster.util;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+
+public class FileUtils {
+
+public FileUtils() {
+}
+
+private void copyFile (String inputFileName, 
+String outputFileName) {
+FileInputStream inFile = null;
+FileOutputStream outFile = null;
+try {
+inFile = new FileInputStream (inputFileName);
+} catch (FileNotFoundException e) {
+new Notify ("Could not open " + inputFileName);
+return;
+}
+try {
+outFile = new FileOutputStream (outputFileName);
+} catch (FileNotFoundException e) {
+new Notify ("Could not open " + outputFileName);
+return;
+}
+byte[] buffer = new byte[1024];
+int length = 0;
+while (length != -1) {
+try {
+length = inFile.read (buffer, 0, buffer.length);
+} catch (IOException e) {
+new Notify ("Problem reading " + inputFileName);
+break;
+}
+if (length == -1) {
+break;
+}
+try {
+outFile.write (buffer, 0, length);
+} catch (IOException e) {
+new Notify ("Problem writing to " + outputFileName);
+break;
+}
+}
+try {
+outFile.close();
+} catch (IOException e) {
+new Notify (outputFileName + " could not be completed");
+}
+}
+}
+

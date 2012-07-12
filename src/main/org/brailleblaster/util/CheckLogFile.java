1064222c8a8f94cd372b647130b66e87195e9af8
John Boyer <john.boyer@abilitiessoft.com>
2012-07-11 23:04:00 -0500

    starting work on CheckLogFile class in util subpackage

---
+++ src/main/org/brailleblaster/util/CheckLogFile.java
@@ -0,0 +1,61 @@
+/* BrailleBlaster Braille Transcription Application
+  *
+  * Copyright (C) 2010, 2012
+  * ViewPlus Technologies, Inc. www.viewplus.com
+  * and
+  * Abilitiessoft, Inc. www.abilitiessoft.com
+  * All rights reserved
+  *
+  * This file may contain code borrowed from files produced by various 
+  * Java development teams. These are gratefully acknoledged.
+  *
+  * This file is free software; you can redistribute it and/or modify it
+  * under the terms of the Apache 2.0 License, as given at
+  * http://www.apache.org/licenses/
+  *
+  * This file is distributed in the hope that it will be useful, but
+  * WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE
+  * See the Apache 2.0 License for more details.
+  *
+  * You should have received a copy of the Apache 2.0 License along with 
+  * this program; see the file LICENSE.
+  * If not, see
+  * http://www.apache.org/licenses/
+  *
+  * Maintained by John J. Boyer john.boyer@abilitiessoft.com
+*/
+
+package org.brailleblaster.util;
+
+import org.eclipse.swt.*;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.MessageBox;
+import org.brailleblaster.BBIni;
+import org.brailleblaster.localization.LocaleHandler;
+
+/**
+ * Check the file liblouisutdml.log in brlblst/temp. If it is not empty 
+ * show its contents. There are separate methods for the GUI and the 
+ * command line.
+ */
+public class CheckLogFile {
+
+public CheckLogFile () {
+}
+
+public void displayLog () {
+String logMessages = null;
+Display display = BBIni.getDisplay();
+Shell shell = new Shell(display, SWT.DIALOG_TRIM);
+MessageBox mb = new MessageBox(shell, SWT.OK);
+mb.setMessage (logMessages);
+mb.open();
+shell.dispose();
+}
+
+public void showLog () {
+}
+
+}

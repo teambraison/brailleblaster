9846abc2a4a38bcf1ff9323e754f29c0f11ed116
John Boyer <john.boyer@abilitiessoft.com>
2012-09-27 12:01:20 -0500

    moving removeDirectory method to FileUtils as deleteDirectory

--- src/main/org/brailleblaster/util/FileUtils.java
+++ src/main/org/brailleblaster/util/FileUtils.java
@@ -105,5 +105,38 @@ public class FileUtils {
         }
     }
 
+	public boolean deleteDirectory(File directory) {
+
+
+		  if (directory == null)
+		    return false;
+		  if (!directory.exists())
+		    return true;
+		  if (!directory.isDirectory())
+		    return false;
+
+		  String[] list = directory.list();
+
+		  // Some JVMs return null for File.list() when the
+		  // directory is empty.
+		  if (list != null) {
+		    for (int i = 0; i < list.length; i++) {
+		      File entry = new File(directory, list[i]);
+
+		      if (entry.isDirectory())
+		      {
+		        if (!deleteDirectory(entry))
+		          return false;
+		      }
+		      else
+		      {
+		        if (!entry.delete())
+		          return false;
+		      }
+		    }
+		  }
+		  return directory.delete();
+	}
+
 }
 

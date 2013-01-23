4c76c47e5411c70c25dad1abd4d5337c4e58d3ee
Brandon Roller <brandon.r.roller@gmail.com>
2013-01-23 13:35:17 -0500

    Removed error I introduced in ant build

--- src/main/org/brailleblaster/BBIni.java
+++ src/main/org/brailleblaster/BBIni.java
@@ -99,9 +99,8 @@ public final class BBIni {
 		long seconds = System.currentTimeMillis() / 1000;
 		instanceId = Long.toString(seconds, 32);
 		LocaleHandler lh = new LocaleHandler();
-		//Main m = new Main();
-		//brailleblasterPath = getBrailleblasterPath(m);
-		brailleblasterPath = "C:\\Users\\broller\\Documents\\LocalTest\\brailleblaster.newdesign\\dist";
+		Main m = new Main();
+		brailleblasterPath = getBrailleblasterPath(m);
 		osName = System.getProperty("os.name");
 		osVersion = System.getProperty("os.version");
 		fileSep = System.getProperty("file.separator");

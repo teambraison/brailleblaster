720b926eb576944b88d6fac6d18bae3b4ee0ca3c
Brandon Roller <brandon.r.roller@gmail.com>
2013-02-06 09:25:04 -0500

    Removed Print Statement used for personal testing

--- src/main/org/brailleblaster/louisutdml/TranslateFile.java
+++ src/main/org/brailleblaster/louisutdml/TranslateFile.java
@@ -7,7 +7,6 @@ public class TranslateFile {
 	public TranslateFile(String configFileList, String inputFileName, String outputFileName, 
 			String logFileName, String settingsString, int mode) throws LiblouisutdmlException {
 					boolean result;
-					System.out.println("LL stuff going on");
 					result = liblouisutdml.getInstance().translateFile(configFileList,
 								inputFileName, outputFileName, logFileName, settingsString, mode);
 	}

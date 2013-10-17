8542bb533ddea44c5b35c5b9cf2a14810e21adad
Brandon Roller <brandon.r.roller@gmail.com>
2013-10-17 13:35:33 -0400

    Added regex statement in Normalizer to remove control chars from within text nodes of poorly formatted documents before translation

--- src/main/org/brailleblaster/document/Normalizer.java
+++ src/main/org/brailleblaster/document/Normalizer.java
@@ -108,7 +108,7 @@ public class Normalizer {
 			else if(list.item(i) instanceof Text){
 				Text t = (Text)list.item(i);
 				String text = t.getTextContent();
-				text = text.replace("\n", " ");
+				text = text.replaceAll("\\s+", " ");
 				t.setTextContent(text);
 			}
 		}

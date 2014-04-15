5e7795e7431f624538b54fe079f5789211dab17e
Keith Creasy <kcreasy@aph.org>
2014-04-15 10:34:57 -0400

    Cleaned up copyright and contact information.

--- src/main/org/brailleblaster/userHelp/UserHelp.java
+++ src/main/org/brailleblaster/userHelp/UserHelp.java
@@ -49,7 +49,7 @@ public class UserHelp {
 		switch (helpChoice) {
 			case AboutBB:
 				new Notify (BBIni.getProductName() + " version " + BBIni.getVersion() +  ", released on " + 
-						BBIni.getReleaseDate() + ". For questions and bug reports contact john.boyer@abilitiessoft.com");
+						BBIni.getReleaseDate() + ". "For more information or to report bugs visit http://www.brailleblaster.org.");
 				break;
 			case HelpInfo:
 				showHelp ("helpinfo.html");

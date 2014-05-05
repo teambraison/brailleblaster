6a457f70258223e857fea9da5a3d2ec350e3b4cf
Sahel "sahel mastoureshgh <Sahel "sahel.mastoureshgh@gmail.com>
2014-05-05 15:22:09 -0400

    added the deleted files

---
+++ src/main/org/brailleblaster/perspectives/webView/WebViewPerspective.java
@@ -0,0 +1,34 @@
+package org.brailleblaster.perspectives.webView;
+
+import org.brailleblaster.perspectives.Perspective;
+import org.brailleblaster.perspectives.webView.WebViewMenu.WebViewMenu;
+import org.brailleblaster.wordprocessor.WPManager;
+/**
+ * Making a perspective class for View view of epub books
+ * following structure of project to extends perspective class
+ * @version:0.0.0
+ * @author smastoureshgh
+ */
+
+public class WebViewPerspective extends Perspective{
+	
+	/**
+	 * Constructor
+	 * @param wp : reference to WPManager class
+	 * @param controller :reference to my controller class 
+	 */
+	public WebViewPerspective(WPManager wp, WebViewController controller){
+		perspectiveType = WebViewController.class;
+		this.controller = controller;
+		this.menu = new WebViewMenu(wp, controller);
+	}
+	
+
+	@Override
+	public void dispose() {
+		menu.dispose();
+	
+		
+	}
+
+}

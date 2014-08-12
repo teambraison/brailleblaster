6432163cb27448402ad7ba90be1558172d07623a
Michael Whapples <Michael Whapples@localhost>
2014-08-12 19:45:39 +0100

    Fix the tabbing issue, it was having SWT.CENTER in the constructor for the Shell, must ensure SWT.CENTER is not used.

--- src/main/org/brailleblaster/wordprocessor/LogViewerDialog.java
+++ src/main/org/brailleblaster/wordprocessor/LogViewerDialog.java
@@ -10,18 +10,24 @@ import org.brailleblaster.localization.LocaleHandler;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.ST;
 import org.eclipse.swt.custom.StyledText;
+import org.eclipse.swt.events.FocusEvent;
+import org.eclipse.swt.events.FocusListener;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.events.TraverseEvent;
+import org.eclipse.swt.events.TraverseListener;
 import org.eclipse.swt.layout.FormLayout;
 import org.eclipse.swt.layout.FormAttachment;
 import org.eclipse.swt.layout.FormData;
 import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Dialog;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.MessageBox;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -30,6 +36,7 @@ public class LogViewerDialog extends Dialog {
 	private LocaleHandler lh;
 	private static Logger log = LoggerFactory.getLogger(LogViewerDialog.class);
 	boolean result = false;
+
 	private static String readFileToString(File inFile) throws IOException {
 		StringBuffer buf = new StringBuffer();
 		String line = null;
@@ -47,27 +54,33 @@ public class LogViewerDialog extends Dialog {
 		}
 		return buf.toString();
 	}
+
 	public LogViewerDialog(Shell parent, int style) {
 		super(parent, style);
 		lh = new LocaleHandler();
 	}
+
 	public LogViewerDialog(Shell parent) {
 		this(parent, SWT.NONE);
 	}
+
 	public boolean open() {
 		Shell parent = getParent();
 		Display display = parent.getDisplay();
-		final Shell dialogShell = new Shell(parent, SWT.DIALOG_TRIM | SWT.APPLICATION_MODAL | SWT.CENTER);
+		final Shell dialogShell = new Shell(parent, SWT.DIALOG_TRIM
+				| SWT.APPLICATION_MODAL);
 		dialogShell.setText(getText());
-		dialogShell.setMinimumSize(300, 400);
 		FormLayout dialogLayout = new FormLayout();
-		dialogShell.setLayout(dialogLayout);;
+		dialogShell.setLayout(dialogLayout);
+		;
+
 		Button closeButton = new Button(dialogShell, SWT.PUSH);
-		FormData closeData = new FormData(30,20);
+		FormData closeData = new FormData(30, 20);
 		closeData.bottom = new FormAttachment(100, -5);
-		closeData.left = new FormAttachment(50,-15);
+		closeData.left = new FormAttachment(50, -15);
 		closeButton.setLayoutData(closeData);
-		closeButton.setText(lh.localValue("buttonClose"));;
+		closeButton.setText(lh.localValue("buttonClose"));
+		;
 		closeButton.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent arg0) {
@@ -75,17 +88,20 @@ public class LogViewerDialog extends Dialog {
 				dialogShell.dispose();
 			}
 		});
-		StyledText logText = new StyledText(dialogShell, SWT.BORDER | SWT.V_SCROLL | SWT.MULTI | SWT.WRAP);
+		StyledText logText = new StyledText(dialogShell, SWT.BORDER
+				| SWT.V_SCROLL | SWT.MULTI | SWT.WRAP);
 		FormData logTextData = new FormData();
 		logTextData.top = new FormAttachment(0, 5);
 		logTextData.left = new FormAttachment(0, 5);
 		logTextData.right = new FormAttachment(100, -5);
 		logTextData.bottom = new FormAttachment(closeButton, -10);
+		logTextData.height = 400;
 		logText.setLayoutData(logTextData);
 		logText.setEditable(false);
 		try {
-		logText.setText(readFileToString(new File(BBIni.getLogFilesPath(), "bb.log")));
-		} catch(IOException e) {
+			logText.setText(readFileToString(new File(BBIni.getLogFilesPath(),
+					"bb.log")));
+		} catch (IOException e) {
 			log.error("Problem opening the log file", e);
 			MessageBox msgBox = new MessageBox(parent, SWT.ICON_ERROR | SWT.OK);
 			msgBox.setText("Unable to open log file");
@@ -94,10 +110,10 @@ public class LogViewerDialog extends Dialog {
 			return result;
 		}
 		logText.setKeyBinding('a' | SWT.MOD1, ST.SELECT_ALL);
-		dialogShell.setTabList(new Control[] {logText, closeButton, logText});
+		
 		dialogShell.pack();
 		dialogShell.open();
-		while(!dialogShell.isDisposed()) {
+		while (!dialogShell.isDisposed()) {
 			if (!display.readAndDispatch()) {
 				display.sleep();
 			}

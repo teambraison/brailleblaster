2e8ca5d4df1e619facd58d69691a7f5179f07602
cmyers <cmyers@cmyers-PC.aphtech.org>
2014-10-20 21:09:42 +0100

    Sash removed. Adjusted views: Tree view is on the left. Text/Braille on the right. Text on top of Braille.

--- src/main/org/brailleblaster/perspectives/braille/views/wp/TextView.java
+++ src/main/org/brailleblaster/perspectives/braille/views/wp/TextView.java
@@ -80,7 +80,7 @@ import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Listener;
 
 public class TextView extends WPView {
-	private final static int LEFT_MARGIN = 20;
+	private final static int LEFT_MARGIN = 21;
 	private final static int RIGHT_MARGIN = 100;
 	private final static int TOP_MARGIN = 0;
 	private final static int BOTTOM_MARGIN = 50;

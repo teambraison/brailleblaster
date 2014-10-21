5d22544adc02314bb6953e58f4c332259d291be6
cmyers <cmyers@cmyers-PC.aphtech.org>
2014-10-21 22:18:01 +0100

    Commented Notify lines.

--- src/main/org/brailleblaster/settings/ui/PagePropertiesTab.java
+++ src/main/org/brailleblaster/settings/ui/PagePropertiesTab.java
@@ -170,17 +170,17 @@ public class PagePropertiesTab {
 			t.addModifyListener(new ModifyListener(){
 				@Override
 				public void modifyText(ModifyEvent e) {
-					Double margin = getDoubleValue(t);
-	
-					if(margin >= getDoubleValue(widthBox) || (getDoubleValue(marginLeftBox) + getDoubleValue(marginRightBox) >= getDoubleValue(widthBox))){
-						new Notify(lh.localValue("incorrectMarginWidth"));
-						t.setText(settingsMap.get(type));
-					}
-					else {
-						settingsMap.put(type, getStringValue(t));
-						cellsBox.setText(String.valueOf(calculateCellsPerLine(Double.valueOf(widthBox.getText()))));
-						linesBox.setText(String.valueOf(calculateLinesPerPage(Double.valueOf(heightBox.getText()))));
-					}
+//					Double margin = getDoubleValue(t);
+//	
+//					if(margin >= getDoubleValue(widthBox) || (getDoubleValue(marginLeftBox) + getDoubleValue(marginRightBox) >= getDoubleValue(widthBox))){
+//						new Notify(lh.localValue("incorrectMarginWidth"));
+//						t.setText(settingsMap.get(type));
+//					}
+//					else {
+//						settingsMap.put(type, getStringValue(t));
+//						cellsBox.setText(String.valueOf(calculateCellsPerLine(Double.valueOf(widthBox.getText()))));
+//						linesBox.setText(String.valueOf(calculateLinesPerPage(Double.valueOf(heightBox.getText()))));
+//					}
 				}		
 			});
 		}
@@ -188,17 +188,17 @@ public class PagePropertiesTab {
 			t.addModifyListener(new ModifyListener(){
 				@Override
 				public void modifyText(ModifyEvent e) {
-					Double margin =  getDoubleValue(t);
-					
-					if(margin >= Double.valueOf(heightBox.getText()) || (getDoubleValue(marginTopBox) + getDoubleValue(marginBottomBox) >= getDoubleValue(widthBox))){
-						new Notify(lh.localValue("incorectMarginHeight"));
-						t.setText(settingsMap.get(type));
-					}
-					else {
-						settingsMap.put(type, getStringValue(t));
-						cellsBox.setText(String.valueOf(calculateCellsPerLine(Double.valueOf(widthBox.getText()))));
-						linesBox.setText(String.valueOf(calculateLinesPerPage(Double.valueOf(heightBox.getText()))));
-					}
+//					Double margin =  getDoubleValue(t);
+//					
+//					if(margin >= Double.valueOf(heightBox.getText()) || (getDoubleValue(marginTopBox) + getDoubleValue(marginBottomBox) >= getDoubleValue(widthBox))){
+//						new Notify(lh.localValue("incorectMarginHeight"));
+//						t.setText(settingsMap.get(type));
+//					}
+//					else {
+//						settingsMap.put(type, getStringValue(t));
+//						cellsBox.setText(String.valueOf(calculateCellsPerLine(Double.valueOf(widthBox.getText()))));
+//						linesBox.setText(String.valueOf(calculateLinesPerPage(Double.valueOf(heightBox.getText()))));
+//					}
 				}		
 			});
 		}

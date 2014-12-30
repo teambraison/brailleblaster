e83223cf18c1dd8688c7602b4f8e4a0405476a6e
Rezylle Milallos <rezyllemilallos@gmail.com>
2014-12-30 13:48:57 -0500

    Print Page Numbers and Braille Page Numbers

---
+++ src/main/org/brailleblaster/archiver/WebArchiver.java
@@ -0,0 +1,132 @@
+package org.brailleblaster.archiver;
+
+import org.brailleblaster.BBIni;
+import org.brailleblaster.document.BBDocument;
+import org.brailleblaster.util.FileUtils;
+import org.brailleblaster.util.Notify;
+
+public class WebArchiver extends Archiver{
+	private final String templateFile = BBIni.getProgramDataPath() + BBIni.getFileSep() + "xmlTemplates" + BBIni.getFileSep() + "textFileTemplate.html";
+	
+	String ext;
+	
+	WebArchiver(String docToPrepare, boolean restore) {
+		super(docToPrepare, restore);
+		currentConfig = getAutoCfg("epub");
+	
+		ext = docToPrepare.substring(docToPrepare.lastIndexOf(".") + 1);
+		filterNames = new String[] {ext, "BRF", "UTDML working document", };
+		filterExtensions = new String[] {"*." + ext, "*.brf", "*.utd", };
+
+		if(workingDocPath.equals(templateFile))
+			originalDocPath = null;
+		
+		copyFileToTemp(docToPrepare);
+	}
+	
+	/**This constructor is used when saveAs is called
+	 * @param oldFile: Path to previous file name used to create a new semantic file
+	 * @param newFile: name of document with new name
+	 */
+	WebArchiver(String oldFile, String newFile, boolean restore) {
+		super(newFile, restore);
+		currentConfig = getAutoCfg("epub");
+	
+		ext = newFile.substring(newFile.lastIndexOf(".") + 1);
+		filterNames = new String[] {ext, "BRF", "UTDML working document", };
+		filterExtensions = new String[] {"*." + ext, "*.brf", "*.utd", };
+
+		if(workingDocPath.equals(templateFile))
+			originalDocPath = null;
+		
+    	copySemanticsForNewFile(oldFile, newFile);
+    	
+		workingDocPath = BBIni.getTempFilesPath() + BBIni.getFileSep() + newFile.substring(newFile.lastIndexOf(BBIni.getFileSep()) + 1);
+	}
+
+	@Override
+	public void save(BBDocument doc, String path) {
+		
+		// Stop the autosave feature until we're done.
+		pauseAutoSave();
+		
+		FileUtils fu = new FileUtils();
+		if(path == null)
+			path = workingDocPath;
+		
+		if(fu.createXMLFile(doc.getNewXML(), workingDocPath)){
+			fu.copyFile(workingDocPath, originalDocPath);
+			String tempSemFile = BBIni.getTempFilesPath() + BBIni.getFileSep() + fu.getFileName(path) + ".sem";
+			String newSemFile = fu.getPath(originalDocPath) + BBIni.getFileSep() + fu.getFileName(originalDocPath) + ".sem";
+			copySemanticsFile(tempSemFile, newSemFile);
+		}
+		else {
+			new Notify("An error occured while saving your document.  Please check your original document.");
+		}
+		
+		// Resume autosave feature.
+		resumeAutoSave(doc, path);
+	}
+
+	private void saveAsWeb(BBDocument doc, String path){
+		// Stop the autosave feature until we're done.
+		pauseAutoSave();
+				
+		String tempFilePath = BBIni.getTempFilesPath() + BBIni.getFileSep() + path.substring(path.lastIndexOf(BBIni.getFileSep()) + 1);
+				
+		FileUtils fu = new FileUtils();
+		if(fu.createXMLFile(doc.getNewXML(), tempFilePath)){
+			fu.copyFile(tempFilePath, path);
+			copySemanticsForNewFile(workingDocPath, path);
+		}
+		else {
+			new Notify("An error occured while saving your document.  Please check your original document.");
+		}
+		
+		originalDocPath = path;
+		workingDocPath = tempFilePath;
+				
+		// Resume autosave feature.
+		resumeAutoSave(doc, path);
+	}
+	
+	@Override
+	public Archiver saveAs(BBDocument doc, String path, String ext) {
+		
+		// Stop the autosave feature until we're done.
+		pauseAutoSave();
+		
+		if(ext.equals(this.ext))
+			saveAsWeb(doc, path);
+		else if(ext.equals("brf"))
+			saveBrf(doc, path);
+		else if(ext.equals("utd"))
+			return saveAsUTD(doc, path);
+		
+		// Resume autosave feature.
+		resumeAutoSave(doc, path);
+		
+		return this;	
+	}
+	
+	private UTDArchiver saveAsUTD(BBDocument doc, String path){
+		
+		// Stop the autosave feature until we're done.
+		pauseAutoSave();
+		
+		UTDArchiver arch = new UTDArchiver(workingDocPath, path, currentConfig, false);
+		arch.save(doc, path);
+		
+		// Resume autosave feature.
+		resumeAutoSave(doc, path);
+		
+		return arch;
+	}
+	
+	//////////////////////////////////////////////////////////////////////////////////
+	// Each Archiver type opens and saves their content in a 
+	// different way. They will implement this method to 
+	// save their content to the temp folder.
+	@Override
+	public void backup(BBDocument __doc, String __path) {}
+}
---
+++ src/main/org/brailleblaster/settings/ui/PageNumbersTab.java
@@ -0,0 +1,236 @@
+package org.brailleblaster.settings.ui;
+
+import java.util.HashMap;
+
+import org.brailleblaster.localization.LocaleHandler;
+import org.brailleblaster.settings.SettingsManager;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.FormAttachment;
+import org.eclipse.swt.layout.FormData;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Combo;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.TabFolder;
+import org.eclipse.swt.widgets.TabItem;
+
+public class PageNumbersTab {
+	
+	HashMap<String, String> settingsMap;
+	SettingsManager sm;
+	TabItem item;
+	LocaleHandler lh;
+	Group ppnGroup, bpnGroup, cpGroup;
+	Composite group;
+	Label cpLabel, intpLabel, ppnRngLabel, bpnLocLabel, ppnLocLabel;
+	//Label ppnLabel, bpnLabel, ppnlocLabel, bpnlocLabel;
+	Combo cpCombo, intpCombo, ppnRngCombo, bpnLocCombo, ppnLocCombo;
+	//Combo ppnCombo, bpnCombo, ppnlocCombo, bpnlocCombo;
+	
+	public PageNumbersTab(TabFolder folder, final SettingsManager sm, HashMap<String, String> settingsMap) {
+		super();
+		lh = new LocaleHandler();
+		this.sm = sm;
+		this.settingsMap = settingsMap;
+		item = new TabItem(folder, 0);	
+		item.setText(lh.localValue("pageNumbers"));
+		
+//		group = new Composite(folder, 0);
+//		group.setLayout(new FormLayout());
+//		item.setControl(group);
+//		setFormLayout(group, 0, 100, 0, 60);
+		 
+		group = new Composite(folder, SWT.BORDER);
+		group.setLayout(new FillLayout(SWT.VERTICAL));
+		item.setControl(group);
+		
+		bpnGroup = new Group(group, 0);
+		bpnGroup.setLayout(new GridLayout(2, true));
+		bpnGroup.setText(lh.localValue("braille"));
+		
+		////////////
+		
+		intpLabel = new Label(bpnGroup, 0);
+		intpLabel.setText("Interpoint");
+		setGridData(intpLabel);
+		intpCombo = new Combo(bpnGroup, SWT.READ_ONLY);
+		intpCombo.add("No");
+		intpCombo.add("Yes");
+		if( settingsMap.get("interpoint").equals("yes") )
+			intpCombo.setText("Yes");
+		else
+			intpCombo.setText("No");
+		setGridData(intpCombo);
+		
+		///////////
+			
+		/**
+		 * Currently the settings have one drop down menu to select "Yes" or "No" 
+		 * and a separate settings box to select the location. 
+		 * Rather than the user having to enter two settings, this could be consolidated into 
+		 * one drop down menu for Braille Page Numbers: Top, Bottom, None.
+		 */
+		
+		bpnLocLabel = new Label (bpnGroup, 0);
+		bpnLocLabel.setText("Braille Page Number Location");
+		setGridData(bpnLocLabel);
+		bpnLocCombo = new Combo(bpnGroup, SWT.READ_ONLY);
+		bpnLocCombo.add("Top");
+		bpnLocCombo.add("Bottom");
+		bpnLocCombo.add("None");
+		
+		if (settingsMap.get("braillePageNumberAt").equals("top")) {
+			bpnLocCombo.setText("Top");
+		}
+		else {
+			bpnLocCombo.setText("Bottom");
+		}
+		
+		if (settingsMap.get("numberBraillePages").equals("no")) {
+			bpnLocCombo.setText("None");
+		}
+		
+		setGridData(bpnLocCombo);
+		
+		/////////////////////////////////////////////////////////////
+		
+		ppnGroup = new Group(group, 0);
+		ppnGroup.setLayout(new GridLayout(2, true));
+		ppnGroup.setText(lh.localValue("print"));
+		
+		ppnLocLabel = new Label(ppnGroup, 0);
+		ppnLocLabel.setText("Print Page Number Location");
+		setGridData(ppnLocLabel);
+		ppnLocCombo = new Combo(ppnGroup, SWT.READ_ONLY);
+		ppnLocCombo.add("Top");
+		ppnLocCombo.add("Bottom");
+		ppnLocCombo.add("None");
+		
+		if (settingsMap.get("printPageNumberAt").equals("top") ) {
+			ppnLocCombo.setText("Top");
+		}
+		else {
+			ppnLocCombo.setText("Bottom");
+		}
+		
+		if (settingsMap.get("printPages").equals("no")) {
+			ppnLocCombo.setText("None");
+		}
+		
+		setGridData(ppnLocCombo);
+		
+		/////////////
+		
+		ppnRngLabel = new Label(ppnGroup, 0);
+		ppnRngLabel.setText("Page Number Range");
+		setGridData(ppnRngLabel);
+		ppnRngCombo = new Combo(ppnGroup, SWT.READ_ONLY);
+		ppnRngCombo.add("No");
+		ppnRngCombo.add("Yes");
+		if( settingsMap.get("printPageNumberRange").equals("yes") )
+			ppnRngCombo.setText("Yes");
+		else
+			ppnRngCombo.setText("No");
+		setGridData(ppnRngCombo);
+		
+		//////////////////////////////////////////////////////////
+		
+		cpGroup = new Group(group, 0);
+		cpGroup.setLayout(new GridLayout(2, true));
+		cpGroup.setText(lh.localValue("continue"));
+		cpLabel = new Label(cpGroup, 0);
+		cpLabel.setText("Continue Pages");
+		setGridData(ppnLocLabel);
+		cpCombo = new Combo(cpGroup, SWT.READ_ONLY);
+		cpCombo.add("No");
+		cpCombo.add("Yes");
+		if( settingsMap.get("continuePages").equals("yes") )
+			cpCombo.setText("Yes");
+		else
+			cpCombo.setText("No");
+		setGridData(cpCombo);
+		
+		
+		addListeners();
+	}
+	
+	private void addListeners(){
+		
+		cpCombo.addSelectionListener(new SelectionAdapter(){
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				int index = cpCombo.getSelectionIndex();
+				settingsMap.put("continuePages", cpCombo.getText().toLowerCase());
+			}
+		});
+		
+		intpCombo.addSelectionListener(new SelectionAdapter(){
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				int index = intpCombo.getSelectionIndex();
+				settingsMap.put("interpoint", intpCombo.getText().toLowerCase());
+			}
+		});
+
+		ppnLocCombo.addSelectionListener(new SelectionAdapter(){
+		@Override
+		public void widgetSelected(SelectionEvent e) {
+			int index = ppnLocCombo.getSelectionIndex();
+			if (ppnLocCombo.getText().equals("None")) {
+				settingsMap.put("printPages", "no");
+			}
+			else {
+				settingsMap.put("printPageNumberAt", ppnLocCombo.getText().toLowerCase());
+				settingsMap.put("printPages", "yes");
+			}
+			}
+		});
+		
+		bpnLocCombo.addSelectionListener(new SelectionAdapter(){
+		@Override
+		public void widgetSelected(SelectionEvent e) {
+			int index = bpnLocCombo.getSelectionIndex();
+			if (bpnLocCombo.getText().equals("None")) {
+				settingsMap.put("numberBraillePages", "no");
+			}
+			else {
+				settingsMap.put("braillePageNumberAt", bpnLocCombo.getText().toLowerCase());
+				settingsMap.put("numberBraillePages", "yes");
+			}
+			}
+		});
+		
+		ppnRngCombo.addSelectionListener(new SelectionAdapter(){
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				int index = ppnRngCombo.getSelectionIndex();
+				settingsMap.put("printPageNumberRange", ppnRngCombo.getText().toLowerCase());
+			}
+		});
+	}
+	
+	private void setFormLayout(Control c, int left, int right, int top, int bottom){
+		FormData location = new FormData();
+		
+		location.left = new FormAttachment(left);
+		location.right = new FormAttachment(right);
+		location.top = new FormAttachment(top);
+		location.bottom = new FormAttachment(bottom);
+		
+		c.setLayoutData(location);
+	}
+	
+	private void setGridData(Control c){
+		GridData gridData = new GridData();
+        gridData.horizontalAlignment = GridData.FILL;
+        gridData.verticalAlignment = GridData.FILL;
+        gridData.grabExcessHorizontalSpace = true;
+        c.setLayoutData(gridData);
+	}
+}

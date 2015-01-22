9f855c39fc35889140f8b7eb5b64405fe054436e
Keith Creasy <kcreasy@gmail.com>
2015-01-22 11:58:57 -0500

    Merged default.

---
+++ src/main/org/brailleblaster/util/ShowBriefly.java
@@ -0,0 +1,63 @@
+/* BrailleBlaster Braille Transcription Application
+ *
+ * Copyright (C) 2014
+* American Printing House for the Blind, Inc. www.aph.org
+* and
+ * ViewPlus Technologies, Inc. www.viewplus.com
+ * and
+ * Abilitiessoft, Inc. www.abilitiessoft.com
+ * All rights reserved
+ *
+ * This file may contain code borrowed from files produced by various 
+ * Java development teams. These are gratefully acknowledged.
+ *
+ * This file is free software; you can redistribute it and/or modify it
+ * under the terms of the Apache 2.0 License, as given at
+ * http://www.apache.org/licenses/
+ *
+ * This file is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE
+ * See the Apache 2.0 License for more details.
+ *
+ * You should have received a copy of the Apache 2.0 License along with 
+ * this program; see the file LICENSE.
+ * If not, see
+ * http://www.apache.org/licenses/
+ *
+ * Maintained by Keith Creasy <kcreasy@aph.org>, Project Manager
+ */
+
+package org.brailleblaster.util;
+
+import org.eclipse.swt.*;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Label;
+import org.brailleblaster.wordprocessor.WPManager;
+
+/**
+ * Show the user a message for a brief time.
+ */
+public class ShowBriefly {
+	private Shell shell;
+
+	public ShowBriefly(String message) {
+		Display display = WPManager.getDisplay();
+		shell = new Shell(display, SWT.DIALOG_TRIM);
+		Label label = new Label(shell, SWT.HORIZONTAL);
+		label.setText(message);
+		shell.open();
+		display.timerExec(60000, new Runnable() {
+			@Override
+			public void run() {
+				endShow();
+			}
+		});
+	}
+
+	private void endShow() {
+		shell.dispose();
+	}
+
+}

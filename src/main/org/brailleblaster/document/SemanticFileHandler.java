4e303de1273c7c03f3e8c95e5fecb613fe6ccb5e
Brandon Roller <brandon.r.roller@gmail.com>
2015-02-20 07:33:36 -0500

    mergd with default

---
+++ src/main/org/brailleblaster/document/SemanticFileHandler.java
@@ -0,0 +1,236 @@
+package org.brailleblaster.document;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map.Entry;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.brailleblaster.BBIni;
+import org.brailleblaster.util.FileUtils;
+
+public class SemanticFileHandler {
+
+	private String configPath;
+	private String defaultSemanticsFiles;
+	private FileUtils fu;
+	private final static Logger log = LoggerFactory.getLogger(SemanticFileHandler.class);
+	private HashMap<String, String> defaults;
+	
+	public SemanticFileHandler(String configPath){
+		fu = new FileUtils();
+		this.configPath = fu.findInProgramData ("liblouisutdml" + BBIni.getFileSep() + "lbu_files" + BBIni.getFileSep() + configPath);
+		this.defaultSemanticsFiles = getSemanticsFile(this.configPath);
+		this.defaults = new HashMap<String, String>();
+		makeDefaultMap();
+	}
+	
+	private String getSemanticsFile(String configPath){
+		String currentLine;
+		FileReader fr;
+		BufferedReader reader;
+		String defaultSem = "*";
+		
+		try {
+			fr = new FileReader(new File(configPath));
+			reader = new BufferedReader(fr);
+		
+			while((currentLine = reader.readLine()) != null){
+				if(!currentLine.contains("#") && currentLine.contains("semanticFiles")){
+					String [] tokens = currentLine.split(" ");
+					defaultSem = tokens[1];
+					break;
+				}
+			}
+			fr.close();
+			reader.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			log.error("File Not Found Exception", e);
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+			log.error("IO Exception", e);
+		}
+		
+		return defaultSem;
+	}
+	
+	private void makeDefaultMap(){		
+		String [] tokens = defaultSemanticsFiles.split(",");
+		for(int i = 0; i < tokens.length; i++){
+			String defaultSemFile = BBIni.getProgramDataPath() + BBIni.getFileSep() + "liblouisutdml" + BBIni.getFileSep() + "lbu_files" + BBIni.getFileSep() + tokens[i];
+			String currentLine;
+			
+			File f = new File(defaultSemFile);
+
+			try {
+				FileReader fr = new FileReader(f);
+				BufferedReader br = new BufferedReader(fr);
+				
+				while ((currentLine = br.readLine()) != null) {
+					if(!currentLine.contains("#") && !currentLine.equals("")){
+						String [] tokens2 = currentLine.split(" ");
+						defaults.put(tokens2[1].trim(), tokens2[0].trim());
+					}
+				}
+				
+				br.close();
+			} 
+			catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+			catch(IOException e){
+				e.printStackTrace();
+			}	
+		}
+	}
+	
+	public String getSemanticsConfigSetting(String filePath){
+		return checkForSemantics(filePath);
+	}
+	
+	private String checkForSemantics(String filePath){
+		String file = filePath.substring(0, filePath.lastIndexOf(".")) + ".sem";
+		String fileName = fu.getFileName(filePath) + ".sem";
+		String tempFile = BBIni.getTempFilesPath() + BBIni.getFileSep() + fileName;
+		if(fu.exists(file)){
+			if(!file.equals(tempFile))
+				fu.copyFile(file, tempFile);
+			
+			return "semanticFiles \"" + defaultSemanticsFiles + "," + tempFile + "\"\n ";
+		}
+		else
+			return "";
+	}
+	
+	public void writeEntry(String path, String style, String element, String id){
+		if(fu.exists(path))
+			appendEntry(path, style, element,id);
+		else 
+			createNewSemanticsFile(path, style, element,id);
+	}
+	
+	public void removeSemanticEntry(String path, String id){
+		if(fu.exists(path))
+			removeEntry(path, id);
+	}
+	
+	private void appendEntry(String path, String style, String element, String id){
+		String text = style + " " + element + ",id," + id + "\n";
+		writeSemanticEntry(path, id, text);
+	}
+	
+	private void createNewSemanticsFile(String path, String style, String element, String id){
+		String text = style + " " + element + ",id," + id + "\n";
+		fu.create(path);
+		fu.writeToFile(path, text);
+	}
+	
+	private void removeEntry(String path, String id){
+		String currentLine;
+    	String [] tokens;
+    	StringBuilder sb= new StringBuilder();
+    	boolean found = false;
+    	
+    	try {
+    		FileReader file = new FileReader(path);
+			BufferedReader reader = new BufferedReader(file);
+			while((currentLine = reader.readLine()) != null){
+				tokens = currentLine.split(",");
+				for(int i = 0; i < tokens.length && !found; i++){
+					if(tokens[i].equals(id))
+						found = true;
+				}
+				
+				if(!found){
+					sb.append(currentLine + "\n");
+				}			
+			}
+			
+			reader.close();
+			fu.writeToFile(path, sb);
+    	}
+    	catch(FileNotFoundException e){
+    		e.printStackTrace();
+    		log.error("File Not Found Exception", e);
+    	}
+    	catch(IOException e){
+    		e.printStackTrace();
+    		log.error("IO Exception", e);
+    	}
+	}
+	
+	private void writeSemanticEntry(String fullPath, String id, String entry){
+		String currentLine;
+    	String [] tokens;
+    	StringBuilder sb= new StringBuilder();
+    	boolean found = false;
+    	boolean entered = false;
+    	
+    	try {
+    		FileReader file = new FileReader(fullPath);
+			BufferedReader reader = new BufferedReader(file);
+			while((currentLine = reader.readLine()) != null){
+				tokens = currentLine.split(",");
+				for(int i = 0; i < tokens.length && !found; i++){
+					if(tokens[i].equals(id))
+						found = true;
+				}
+				
+				if(found && !entered){
+					sb.append(entry);
+					entered = true;
+				}
+				else {
+					sb.append(currentLine + "\n");
+				}				
+			}
+			
+			if(!found){
+				sb.append(entry);
+			}
+			reader.close();
+			fu.writeToFile(fullPath, sb);
+    	}
+    	catch(FileNotFoundException e){
+    		e.printStackTrace();
+    		log.error("File Not Found Exception", e);
+    	}
+    	catch(IOException e){
+    		e.printStackTrace();
+    		log.error("IO Exception", e);
+    	}
+	}
+	
+	public void resetSemanticHandler(String config){
+		defaults.clear();
+		configPath = fu.findInProgramData ("liblouisutdml" + BBIni.getFileSep() + "lbu_files" + BBIni.getFileSep() + config);
+		defaultSemanticsFiles = getSemanticsFile(this.configPath);
+		makeDefaultMap();
+	}
+	
+	public String getDefaultSemanticsFiles(){
+		return defaultSemanticsFiles;
+	}
+	
+	public String getDefault(String elementName){
+		return defaults.get(elementName);
+	}
+	
+	public String getElementBySemantic(String semantic){
+		return getKeyByValue(semantic);
+	}
+			
+	private String getKeyByValue(String sem) {
+		for (Entry<String, String> entry : defaults.entrySet()) {
+			if (entry.getValue().equals(sem))
+				return entry.getKey();
+		}
+			return null;
+	}
+}

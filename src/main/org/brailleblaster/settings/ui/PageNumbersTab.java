83df65d7d10cd8477cff36b97605a63631008241
Leon Blakey <lblakey@aph.org>
2015-03-16 15:56:05 -0400

    Un-merged NumberPosition fields in Page Numbers tab, updated with new UTD changes

--- src/main/org/brailleblaster/settings/ui/PageNumbersTab.java
+++ src/main/org/brailleblaster/settings/ui/PageNumbersTab.java
@@ -2,8 +2,9 @@ package org.brailleblaster.settings.ui;
 
 import org.brailleblaster.localization.LocaleHandler;
 import org.brailleblaster.utd.PageSettings;
-import org.brailleblaster.utd.PageSettings.NumberLocation;
 import org.brailleblaster.utd.UTDTranslationEngine;
+import org.brailleblaster.utd.properties.PageNumberPosition;
+import org.brailleblaster.utd.properties.PageNumberType;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Combo;
@@ -14,7 +15,7 @@ import org.eclipse.swt.widgets.TabItem;
 
 class PageNumbersTab implements SettingsUITab {
 	private final LocaleHandler lh = new LocaleHandler();
-	private final Combo continueCombo, interpointCombo, printCombo;
+	private final Combo interpointCombo, brailleNumCombo, printNumCombo, continueSymbolsCombo, continuePagesCombo;
 
 	PageNumbersTab(TabFolder folder, PageSettings pageSettingsDefault) {
 		TabItem item = new TabItem(folder, 0);
@@ -25,33 +26,39 @@ class PageNumbersTab implements SettingsUITab {
 		parent.setLayout(new GridLayout(1, true));
 		item.setControl(parent);
 
-		//Braille interpoint and continuation symbols
-		Group pageNumGroup = new Group(parent, 0);
-		pageNumGroup.setLayout(new GridLayout(2, true));
-		pageNumGroup.setText("Page Numbers");
-		SettingsUIUtils.setGridDataGroup(pageNumGroup);
+		//Braille group
+		Group brailleGroup = new Group(parent, 0);
+		brailleGroup.setLayout(new GridLayout(2, true));
+		brailleGroup.setText(lh.localValue("braille"));
+		SettingsUIUtils.setGridDataGroup(brailleGroup);
 
-		SettingsUIUtils.addLabel(pageNumGroup, "Braille Interpoint Page Number Location");
-		interpointCombo = makeNumberPositionCombo(pageNumGroup, pageSettingsDefault.getInterpoint());
+		SettingsUIUtils.addLabel(brailleGroup, "Interpoint");
+		interpointCombo = makeYesNoCombo(brailleGroup, pageSettingsDefault.getInterpoint());
 
-		SettingsUIUtils.addLabel(pageNumGroup, "Continuation Symbols Print Page Number Location");
-		printCombo = makeNumberPositionCombo(pageNumGroup, pageSettingsDefault.getPrintPages());
+		SettingsUIUtils.addLabel(brailleGroup, "Braille Page Number Location");
+		brailleNumCombo = makeNumberPositionCombo(brailleGroup, pageSettingsDefault.getBraillePageNumberAt());
 
-		//Continue pages
+		//Print group
+		Group printGroup = new Group(parent, 0);
+		printGroup.setLayout(new GridLayout(2, true));
+		printGroup.setText(lh.localValue("print"));
+		SettingsUIUtils.setGridDataGroup(printGroup);
+
+		SettingsUIUtils.addLabel(printGroup, "Print Page Number Location");
+		printNumCombo = makeNumberPositionCombo(printGroup, pageSettingsDefault.getPrintPageNumberAt());
+
+		SettingsUIUtils.addLabel(printGroup, "Continuation Symbols For Print Pages");
+		continueSymbolsCombo = makeYesNoCombo(printGroup, pageSettingsDefault.isPrintPageNumberRange());
+
+		//Continue pages group
 		Group cpGroup = new Group(parent, 0);
 		cpGroup.setLayout(new GridLayout(2, true));
 		cpGroup.setText(lh.localValue("continue"));
 		SettingsUIUtils.setGridDataGroup(cpGroup);
 
 		SettingsUIUtils.addLabel(cpGroup, "Continue Pages");
-		continueCombo = new Combo(cpGroup, SWT.READ_ONLY);
-		continueCombo.add("No");
-		continueCombo.add("Yes");
-		if (pageSettingsDefault.isContinuePages())
-			continueCombo.setText("Yes");
-		else
-			continueCombo.setText("No");
-		SettingsUIUtils.setGridData(continueCombo);
+		continuePagesCombo = makeYesNoCombo(cpGroup, pageSettingsDefault.isContinuePages());
+		SettingsUIUtils.setGridData(continuePagesCombo);
 	}
 
 	@Override
@@ -66,21 +73,43 @@ class PageNumbersTab implements SettingsUITab {
 		boolean updated = false;
 
 		updated = SettingsUIUtils.updateObject(pageSettings::getInterpoint, pageSettings::setInterpoint,
-				NumberLocation.valueOf(interpointCombo.getText()), updated);
-		updated = SettingsUIUtils.updateObject(pageSettings::getPrintPages, pageSettings::setPrintPages,
-				NumberLocation.valueOf(printCombo.getText()), updated);
+				interpointCombo.getText().equals("Yes"), updated);
+		updated = SettingsUIUtils.updateObject(pageSettings::getBraillePageNumberAt, pageSettings::setBraillePageNumberAt,
+				PageNumberPosition.valueOf(brailleNumCombo.getText()), updated);
+		updated = SettingsUIUtils.updateObject(pageSettings::getPrintPageNumberAt, pageSettings::setPrintPageNumberAt,
+				PageNumberPosition.valueOf(printNumCombo.getText()), updated);
+		updated = SettingsUIUtils.updateObject(pageSettings::isPrintPageNumberRange, pageSettings::setPrintPageNumberRange,
+				continueSymbolsCombo.getText().equals("Yes"), updated);
 		updated = SettingsUIUtils.updateObject(pageSettings::isContinuePages, pageSettings::setContinuePages,
-				continueCombo.getText().equals("Yes"), updated);
+				continuePagesCombo.getText().equals("Yes"), updated);
 
 		return updated;
 	}
 
-	private static Combo makeNumberPositionCombo(Composite parent, NumberLocation defaultValue) {
+	private static boolean isComboYes(Combo combo) {
+		return combo.getText().equals("Yes");
+	}
+
+	private static Combo makeNumberPositionCombo(Composite parent, PageNumberPosition defaultValue) {
 		Combo combo = new Combo(parent, SWT.READ_ONLY);
 
-		for (NumberLocation curLoc : NumberLocation.values())
+		for (PageNumberPosition curLoc : PageNumberPosition.values())
 			combo.add(curLoc.name());
 		combo.setText(defaultValue.name());
+
+		SettingsUIUtils.setGridData(combo);
+		return combo;
+	}
+
+	private static Combo makeYesNoCombo(Composite parent, boolean defaultValue) {
+		Combo combo = new Combo(parent, SWT.READ_ONLY);
+		combo.add("Yes");
+		combo.add("No");
+
+		if (defaultValue)
+			combo.setText("Yes");
+		else
+			combo.setText("No");
 
 		SettingsUIUtils.setGridData(combo);
 		return combo;

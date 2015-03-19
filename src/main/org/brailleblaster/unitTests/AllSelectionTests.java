466be5946a7b11801f20dbbec944dcdce286b7a6
Brandon Roller <brandon.r.roller@gmail.com>
2015-03-19 10:06:11 -0400

    Added more integration tests for editing and style changes

--- src/main/org/brailleblaster/unitTests/AllSelectionTests.java
+++ src/main/org/brailleblaster/unitTests/AllSelectionTests.java
@@ -5,7 +5,8 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ ReadOnlyCursorTests.class, BoxlineTests.class, BoxlineSelectionTests.class})
+@SuiteClasses({ ReadOnlyCursorTests.class, BoxlineTests.class, BoxlineSelectionTests.class, StyleChangeTests.class, EditingTests.class, PageSelectionTests.class,
+	HideActionTests.class})
 
 public class AllSelectionTests {  
 	
